2e210a978b30037fb0e2c7601398f70cf4b84cc6
TUR1412 <1311898171@qq.com>
2026-01-13 08:24:16 +0800

    feat(ui): microinteractions for docs/back-to-top

--- scripts.js
+++ scripts.js
@@ -2383,9 +2383,9 @@
     }
 
     const INTERACT_SELECTOR =
-      ".btn, .btn-small, .icon-button, .chip, .tag, .mobile-nav-toggle, header nav a, .save-pill, .filter-chip, .view-btn, .cmdk-item, .search-btn, .select-pill, .filter-option, .toggle-pill, .checklist-item";
+      ".btn, .btn-small, .icon-button, .chip, .tag, .mobile-nav-toggle, header nav a, .skip-link, .docs-nav-link, .back-to-top, .compare-close, .diag-close, .save-pill, .filter-chip, .view-btn, .cmdk-item, .search-btn, .select-pill, .filter-option, .toggle-pill, .checklist-item";
     const MAGNETIC_SELECTOR =
-      ".btn, .btn-small, .icon-button, .chip, .tag, .mobile-nav-toggle, .save-pill, .filter-chip, .view-btn, .search-btn";
+      ".btn, .btn-small, .icon-button, .chip, .tag, .mobile-nav-toggle, .back-to-top, .save-pill, .filter-chip, .view-btn, .search-btn";
 
     const isPrimaryPointer = (e) => {
       if (!e) return false;
